899c0e16cef5777af85d7dc28fe893186250f127
sfrwmaker <sfrwmaker@gmail.com>
2022-10-06 21:44:10 +0300

    Add files via upload

---
+++ FatFS/diskio.c
@@ -0,0 +1,162 @@
+/*-----------------------------------------------------------------------*/
+/* Low level disk I/O module skeleton for FatFs     (C)ChaN, 2019        */
+/*-----------------------------------------------------------------------*/
+/* If a working storage control module is available, it should be        */
+/* attached to the FatFs via a glue function rather than modifying it.   */
+/* This is an example of glue functions to attach various exsisting      */
+/* storage control modules to the FatFs module with a defined API.       */
+/*-----------------------------------------------------------------------*/
+
+#include "ff.h"			/* Obtains integer types */
+#include "diskio.h"		/* Declarations of disk functions */
+#include "sdspi.h"
+
+/* Definitions of physical drive number for each drive */
+#define DEV_SDCARD	(0)
+
+SDCARD sd;
+
+/*-----------------------------------------------------------------------*/
+/* Get Drive Status                                                      */
+/*-----------------------------------------------------------------------*/
+
+DSTATUS disk_status (
+	BYTE pdrv		/* Physical drive number to identify the drive */
+)
+{
+
+	switch (pdrv) {
+	case DEV_SDCARD:
+		return 0;
+		break;
+	}
+	return STA_NOINIT;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Inidialize a Drive                                                    */
+/*-----------------------------------------------------------------------*/
+
+DSTATUS disk_initialize (
+	BYTE pdrv				/* Physical drive number to identify the drive */
+)
+{
+
+	switch (pdrv) {
+	case DEV_SDCARD:
+		if (SD_Init(&sd) == 0)
+			return 0;
+		break;
+	}
+	return STA_NOINIT;
+
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Read Sector(s)                                                        */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_read (
+	BYTE pdrv,		/* Physical drive number to identify the drive */
+	BYTE *buff,		/* Data buffer to store read data */
+	LBA_t sector,	/* Start sector in LBA */
+	UINT count		/* Number of sectors to read */
+)
+{
+	DRESULT res	 = RES_OK;
+	uint8_t code = 0;
+
+	switch (pdrv) {
+	case DEV_SDCARD:
+		code = SD_Read(&sd, sector, count, buff);
+		if (code >= 2)
+			res = RES_ERROR;
+		else if (code == 1)
+			res = RES_NOTRDY;
+		return res;
+	}
+	return RES_PARERR;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Write Sector(s)                                                       */
+/*-----------------------------------------------------------------------*/
+
+#if FF_FS_READONLY == 0
+
+DRESULT disk_write (
+	BYTE pdrv,			/* Physical drive number to identify the drive */
+	const BYTE *buff,	/* Data to be written */
+	LBA_t sector,		/* Start sector in LBA */
+	UINT count			/* Number of sectors to write */
+)
+{
+	DRESULT res  = RES_OK;
+	uint8_t code = 0;
+
+	switch (pdrv) {
+	case DEV_SDCARD:
+		code = SD_Write(&sd, sector, count, buff);
+		if (code >= 2)
+			res = RES_ERROR;
+		else if (code == 1)
+			res = RES_NOTRDY;
+		return res;
+	}
+	return RES_PARERR;
+}
+
+#endif
+
+
+/*-----------------------------------------------------------------------*/
+/* Miscellaneous Functions                                               */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_ioctl (
+	BYTE pdrv,		/* Physical drive number (0..) */
+	BYTE cmd,		/* Control code */
+	void *buff		/* Buffer to send/receive control data */
+)
+{
+	DRESULT res = RES_OK;
+
+	if (pdrv == DEV_SDCARD) {
+		switch (cmd) {
+		case CTRL_SYNC:
+			res = RES_OK;
+			break;
+		case GET_SECTOR_COUNT:
+			{
+			LBA_t *sc = buff;
+			*sc =  sd.blocks; 	/* 512 bytes sector number */
+			}
+			break;
+		case GET_SECTOR_SIZE:
+			{
+			WORD *ss = buff;
+			*ss = 512;
+			}
+			break;
+		case GET_BLOCK_SIZE:
+			{
+				DWORD *sz_blk = buff;
+				*sz_blk = 1;	/* sectors */
+			}
+			break;
+		case CTRL_TRIM:
+		default:
+			res = RES_ERROR;
+			break;
+		}
+		return res;
+	}
+	return RES_PARERR;
+}
+
