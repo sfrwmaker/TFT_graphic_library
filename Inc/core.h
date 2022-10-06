b88b17afea640109667c8ddaad07f637d737de87
sfrwmaker <sfrwmaker@gmail.com>
2022-10-06 21:41:51 +0300

    Add files via upload

---
+++ Inc/core.h
@@ -0,0 +1,28 @@
+/*
+ * core.h
+ *
+ *  Created on: 16 sep 2019
+ *      Author: Alex
+ */
+
+#ifndef CORE_H_
+#define CORE_H_
+
+#include "main.h"
+
+#ifndef __cplusplus
+typedef	uint8_t	bool;
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void setup(void);
+void loop(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ Inc/main.h
@@ -0,0 +1,92 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define TOUCH_CS_Pin GPIO_PIN_3
+#define TOUCH_CS_GPIO_Port GPIOA
+#define TOUCH_IRQ_Pin GPIO_PIN_4
+#define TOUCH_IRQ_GPIO_Port GPIOA
+#define TFT_SCK_Pin GPIO_PIN_5
+#define TFT_SCK_GPIO_Port GPIOA
+#define TOUCH_MISO_Pin GPIO_PIN_6
+#define TOUCH_MISO_GPIO_Port GPIOA
+#define TFT_SDI_Pin GPIO_PIN_7
+#define TFT_SDI_GPIO_Port GPIOA
+#define TFT_CS_Pin GPIO_PIN_12
+#define TFT_CS_GPIO_Port GPIOB
+#define TFT_DC_Pin GPIO_PIN_13
+#define TFT_DC_GPIO_Port GPIOB
+#define TFT_RESET_Pin GPIO_PIN_14
+#define TFT_RESET_GPIO_Port GPIOB
+#define SD_CS_Pin GPIO_PIN_15
+#define SD_CS_GPIO_Port GPIOA
+#define SD_SCK_Pin GPIO_PIN_3
+#define SD_SCK_GPIO_Port GPIOB
+#define SD_MISO_Pin GPIO_PIN_4
+#define SD_MISO_GPIO_Port GPIOB
+#define SD_MOSI_Pin GPIO_PIN_5
+#define SD_MOSI_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
